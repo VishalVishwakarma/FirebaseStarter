ab8c794c75395a9d03f407e2f2aa61fcd4a15204
VishalVishwakarma <vishalvishwakarma2010@hotmail.com>
2017-03-26 00:18:01 +0530

    commit - line space deleted

--- app/src/main/java/com/example/vishal/firebasestarter/MainActivity.java
+++ app/src/main/java/com/example/vishal/firebasestarter/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Get UI Elements
-
         mConditionTextView = (TextView)findViewById(R.id.textViewCondition);
 
     }
